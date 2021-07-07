dcd249ec3c80e43ab79b5ba9ac60469abc8d66c6
Shogo Makio <makio.shogo@gmail.com>
2021-07-08 00:09:45 +0900

    add: rooms table

---
+++ database/migrations/2021_07_07_150909_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable(false);
+            $table->tinyInteger('type')->nullable(false)->default(1);
+            $table->tinyInteger('hidden')->nullable(false)->default(0);
+            $table->tinyInteger('posting_allowed')->nullable(false)->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
